6f646b4d2a1ddefc0f73876b8e4ab20ffb90672e
Yaslam Salman Sando <123530420+salamsando@users.noreply.github.com>
2025-11-21 16:57:11 +0000

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let myName = "Yaslam Salman Sando";
+let myAge = 27;          
+let likesJavascript = true;      
+
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Likes JavaScript:",likesJavascript);
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition result:", num1 + num2);
+console.log("Subtraction result:", num1 - num2);
+console.log("Multiplication result:", num1 * num2);
+console.log("Division result:", num1 / num2);
+
+if (myAge >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+let favoriteColor = prompt("What is your favorite color?");
+console.log("Your favorite color is:", favoriteColor);
